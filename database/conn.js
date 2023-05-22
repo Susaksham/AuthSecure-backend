01335b824bae028e847f900dcafc32f8c0062a77
Susaksham Jain <sakshamyogesh@gmail.com>
2023-05-22 20:22:10 +0530

    backend updated

--- database/conn.js
+++ database/conn.js
@@ -1,21 +1,23 @@
-import mongoose from 'mongoose'
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import ENV from '../config.js'
-import * as dotenv from 'dotenv';
-dotenv.config()
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import ENV from "../config.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function connect() {
   try {
-    const mongo = await MongoMemoryServer.create()
-    const getUri = mongo.getUri()
-    mongoose.set('strictQuery', true)
+    const mongo = await MongoMemoryServer.create();
+    const getUri = mongo.getUri();
+    mongoose.set("strictQuery", true);
     // const db = await mongoose.connect(getUri)
-  
-    const db = await mongoose.connect(`mongodb+srv://saksham:${process.env. ATLAS_PASS}@cluster0.y3iucbq.mongodb.net/?retryWrites=true&w=majority`)
-    console.log('Database connected')
-    return db
+
+    const db = await mongoose.connect(
+      `mongodb+srv://saksham:${process.env.ATLAS_PASS}@cluster0.y3iucbq.mongodb.net/?retryWrites=true&w=majority`
+    );
+    console.log("Database connected");
+    return db;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
-export default connect
+export default connect;
